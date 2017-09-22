05108493b68c6867bdfab979f133728fc4a10564
Paul Marganian <pmargani@nrao.edu>
2017-09-22 11:27:49 -0400

    first pass at highest level function for calibrating DCR data

--- src/calibrateDcrData.py
+++ src/calibrateDcrData.py
@@ -1,4 +1,40 @@
+from dcr_decode import decode
+from rcvr_table import ReceiverTable
+from calibrator import TraditionalCalibrator, WBandCalibrator, ArgusCalibrator, KaCalibrator
 
 
 def calibrateDcrData(projPath, scanNum, calMode, polMode):
-	pass
+    rcvrTable = ReceiverTable.load("rcvrTable.csv")
+    
+    table = decode(projPath, scanNum)
+    receiver = table.meta['RECEIVER']
+
+    mask = rcvrTable['M&C Name'] == receiver
+
+    acceptableCalModes = rcvrTable.meta['calibrationOptions'].values()
+    acceptablePolModes = rcvrTable.meta['calibrationOptions'].values()
+
+    if calMode not in rcvrTable.meta['calibrationOptions'].values():
+        raise ValueError("calMode {} is invalid. Must be one of {}"
+                         .format(calMode, ))
+
+    rcvrRows = rcvrTable[mask]
+
+    if len(rcvrRows) != 1:
+        raise ValueError("There should be exactly one row in the Receiver "
+                         "Table for receiver {}".format(receiver))
+
+    rcvrRow = rcvrRows[0]
+    calClass = eval(rcvrRow['Calibration Strategy'])
+
+    import ipdb; ipdb.set_trace()
+
+    doGain = calMode != "Raw"
+
+
+if __name__=="__main__":
+    projPath = "../test/data/AGBT16B_285_01"
+    scanNum = 5
+    calMode = "Total Power"
+    polMode = 'X/L'
+    calibrateDcrData(projPath, scanNum, calMode, polMode)
