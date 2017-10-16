4382cb04d59d194e3c0dc3615e18fed17523044d
Paul Marganian <pmargani@nrao.edu>
2017-10-16 11:38:55 -0400

    Enhanced Jenkinsfile to install virtualenv

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,14 @@
+// This is the name given to the virtual environment for
+// this build
+venv_name = "testing-gbtcal-env"
+
+def installPythonPackages() {
+
+    sh """virtualenv -p /opt/local/bin/python2.7 ${venv_name}
+    source ${venv_name}/bin/activate
+    pip install -U pip setuptools
+    pip install -r requirements.txt"""
+}
 
 node {
     stage('cleanup') {
@@ -6,5 +17,14 @@ node {
 
     stage('checkout') {
         checkout scm
+    }
+
+    stage('install') {
+        try {
+            installPythonPackages()
+        } catch(error) {
+            notify(failure, 'An error has occurred during the <b>install</b> stage.')
+            throw(error)
+        }
     }
 }    
