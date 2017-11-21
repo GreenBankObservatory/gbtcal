c5475bb53ebb0e7c58da28f9b14743547433c3fe
Nathaniel Sizemore <nsizemor@nrao.edu>
2017-11-21 15:54:36 -0500

    Added missing functions to Jenkinsfile.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,27 @@
+def lastCommit() {
+    sh 'git log --format="%ae" | head -1 > commit-author.txt'                 
+    lastAuthor = readFile('commit-author.txt').trim()
+    sh 'rm commit-author.txt'
+    return lastAuthor
+}
+
+// Used to send email notification of success or failure
+def notify(status, details){
+    def failure_description = "";
+    def lastChangedBy = lastCommit()
+    if (status == 'failure') {
+        failure_description = """${env.BUILD_NUMBER} failed."""
+    }
+    emailext (
+      to: "sddev@nrao.edu",
+      subject: "${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+      body: """${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':
+        ${failure_description}
+        Last commit by: ${lastChangedBy}
+        Build summary: ${env.BUILD_URL}"""
+    )
+}
+
 // This is the name given to the virtual environment for
 // this build
 venv_name = "testing-gbtcal-env"
